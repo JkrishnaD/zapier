35bc10c7dd0982340b536d80dd1924023c66a13e
JKrishnaD <jayak5063@gmail.com>
2024-11-05 15:21:33 +0530

    zap-ui-change

--- apps/web/components/zap-cell.tsx
+++ apps/web/components/zap-cell.tsx
@@ -11,12 +11,12 @@ export const ZapCell = ({ name, index, onClick, image }: ZapCellProps) => {
     <div className="flex items-center justify-center flex-col">
       <div
         onClick={onClick}
-        className="border-2 border-slate-200 flex justify-center max-w-md w-[300px] cursor-pointer py-4 px-20 rounded-md bg-white hover:bg-white/60"
+        className="border-2 border-slate-200 flex justify-center max-w-md w-[400px] cursor-pointer py-4 px-20 rounded-md bg-white hover:bg-white/60"
       >
-        <div className="flex flex-row text-xl gap-x-1">
+        <div className="flex flex-row text-xl gap-x-1 items-center">
           <p className="font-bold">{index}.</p>
-          {image ? <Image src={image} alt="" width={20} height={10} /> : null}
-          <p>{name}</p>
+          {image ? <Image src={image} alt="" width={25} height={20} /> : null}
+          <p className="flex flex-row items-center">{name}</p>
         </div>
       </div>
       <IoIosArrowRoundDown size={25} className="mb-1" />
